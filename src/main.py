0eac55ab355d0f6a001a876bba05f018924c8cb2
MayaVB <mayaveismman@gmail.com>
2024-08-20 13:04:24 +0300

    added main.py in addition to notebook main

---
+++ src/main.py
@@ -0,0 +1,55 @@
+
+from sklearn.impute import KNNImputer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+from eval import plot_feature_importances, plot_auc_roc_curve, perform_cross_validation
+from getdata import add_nasdaq_annual_changes, add_economic_indicators
+from models import train_xgb_model, train_rf_model, train_svm_model
+from models import evaluate_model, predict_model
+from printstatistics import print_correlations_Spearman_and_Pearson
+from preprocess import load_data, preprocess_data
+
+########################## MAIN ##########################
+df = load_data('data/startup_data.csv')
+
+# Add economic indicators
+df = add_nasdaq_annual_changes(df)
+indicator_code = 'NY.GDP.MKTP.KD.ZG'
+df = add_economic_indicators(df, indicator_code)
+indicator_code = 'SL.UEM.TOTL.ZS'
+df = add_economic_indicators(df, indicator_code)
+
+# Preprocess the data
+X, y = preprocess_data(df)
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
+
+# Train predict and evaluate models
+# XG Boost
+xgb_clf = train_xgb_model(X_train, y_train)
+xgb_pred, xgb_prob = predict_model(xgb_clf, X_test)
+evaluate_model(y_test, xgb_pred, xgb_prob, threshold=0.7)
+cv_results_xgb = perform_cross_validation(xgb_clf, X_train, y_train, n_splits=5, random_state=42)
+
+plot_feature_importances(xgb_clf, feature_names=X.columns, num_of_features=10)
+plot_auc_roc_curve(y_test, xgb_prob, model_name='XG-Boost')
+
+# Fandom Forest
+rf_clf = train_rf_model(X_train, y_train)
+rf_pred, rf_prob = predict_model(rf_clf, X_test)
+evaluate_model(y_test, rf_pred, rf_prob)
+cv_results_rf = perform_cross_validation(rf_clf, X_train, y_train, n_splits=5, random_state=42)
+
+plot_feature_importances(rf_clf, feature_names=X.columns, num_of_features=10)
+plot_auc_roc_curve(y_test, rf_prob, model_name='randomForest')
+
+# SVM
+svm_clf = train_svm_model(X_train, y_train)
+svm_pred, svm_prob = predict_model(svm_clf, X_test)
+evaluate_model(y_test, svm_pred, svm_prob)
+cv_results_SVM = perform_cross_validation(svm_clf, X_train, y_train, n_splits=5, random_state=42)
+
+plot_auc_roc_curve(y_test, svm_prob, model_name='SVM')
+
