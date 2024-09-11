cff6f954835d5a6c4fef8fe6492cf9f6eea3b18c
MayaVB <mayaveismman@gmail.com>
2024-09-11 16:34:10 +0300

    bugfix in matrics calculation

--- src/models.py
+++ src/models.py
@@ -217,20 +217,23 @@ def cross_validate_model_using_StratifiedKFold(model, X, y, n_splits=5, random_s
         y_pred = model.predict(X_val_cv)
         y_prob = model.predict_proba(X_val_cv)[:, 1]  # Assuming binary classification
 
+        # Convert probabilities to binary predictions based on threshold
+        y_pred_thresh = (y_prob >= th_val).astype(int)
+
         # Evaluate model
         kappa = cohen_kappa_score(y_val_cv, y_pred)
         kappa_scores.append(kappa)
 
-        accuracy = accuracy_score(y_val_cv, y_prob<th_val)
+        accuracy = accuracy_score(y_val_cv, y_pred_thresh)
         accuracy_scores.append(accuracy)
 
-        balanced_acc = balanced_accuracy_score(y_val_cv, y_prob<th_val)
+        balanced_acc = balanced_accuracy_score(y_val_cv, y_pred_thresh)
         balanced_acc_scores.append(balanced_acc)
 
-        precision = precision_score(y_val_cv, y_prob<th_val)
+        precision = precision_score(y_val_cv, y_pred_thresh)
         precision_scores.append(precision)
-        
-        recall = recall_score(y_val_cv, y_prob<th_val)
+
+        recall = recall_score(y_val_cv, y_pred_thresh)
         recall_scores.append(recall)
 
         # Collect confusion matrix for each fold
